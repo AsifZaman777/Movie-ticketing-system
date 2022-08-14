acf430b1bee67461580307233015e9c1efff3a3c
Asif Zaman <eramasif16@gmail.com>
2022-08-14 14:42:49 +0600

    added js

--- user_view/homepage.js
+++ user_view/homepage.js
@@ -1,4 +1,5 @@
 let popup=document.getElementById("popup");
+let bookPopup=document.getElementById("bookPopup");
 
 function openPopup(){
     popup.classList.add("openPopup");
@@ -6,3 +7,34 @@ function openPopup(){
 function closePopup(){
     popup.classList.remove("openPopup");
 }
+
+function openBook()
+{
+   bookPopup.classList.add("openBook");
+}
+function closeBook()
+{
+   bookPopup.classList.remove("openBook");
+}
+
+function hawa()
+{
+    document.getElementById("mname").value="Hawa";
+}
+
+function poran()
+{
+    document.getElementById("mname").value="Poran";
+}
+
+function bullet()
+{
+    document.getElementById("mname").value="Bullet Train";
+}
+
+function love()
+{
+    document.getElementById("mname").value="Love and Thunder";
+}
+
+
