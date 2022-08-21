5efec8f031d8e8cc7a262d4a5bd5221024fa3bc1
Asif Zaman <eramasif16@gmail.com>
2022-08-21 14:56:50 +0600

    added primary handle

--- user_controller/homepage_valid.php
+++ user_controller/homepage_valid.php
@@ -11,7 +11,18 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     $movie_email=$_POST['email'];
     $vanue=$_POST['mvanue'];
    
-        
+    $obj=new db();
+    $conn=$obj->opencon();
+    $tablename="booking";
+
+    $check_email=$conn->query("SELECT email FROM $tablename WHERE email='$movie_email'");
+
+    if($check_email->num_rows>0)
+    {
+        echo "<p>You have already a booking</p>";
+    }
+    else
+    {
         if(empty($movie_name) || empty($movie_date) || empty($movie_email) || empty($vanue))
         {
             echo "<p>Please fill all the fields</p>";
@@ -25,8 +36,9 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
         }
        
 }
-
-
+    }
+        
+        
 }
 
 
